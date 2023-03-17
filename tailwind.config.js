4c4e21fcc61107a2c787c80d3243afedf36ccf9b
MohammedAlzayyan <mohammedznn1999@gmail.com>
2023-03-17 21:04:38 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './src/pages/**/*.{html,js,jsx}',
     './src/components/**/*.{html,js,jsx}',
     './src/layouts/**/*.{html,js,jsx}',
+    './src/sections/**/*.{html,js,jsx}',
   ],
   theme: {
     extend: {},
