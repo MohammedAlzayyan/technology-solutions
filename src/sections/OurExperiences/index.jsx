2cd271bc59775ca45de330a7d682d8b527851f4d
MohammedAlzayyan <mohammedznn1999@gmail.com>
2023-03-19 20:18:59 +0300

    updating

--- src/sections/OurExperiences/index.jsx
+++ src/sections/OurExperiences/index.jsx
@@ -5,7 +5,7 @@ import Card from "../../components/Card";
 
 const DevelopBusiness = () => {
   return (
-    <div className="flex flex-col gap-4 px-32 bg-gray-100">
+    <div className="flex flex-col gap-4 px-32 py-7 bg-gray-100">
       <div className="flex  gap-4 ">
         <div className="flex flex-col gap-7 flex-1 justify-center">
           <h2 className="text-3xl font-semibold">
@@ -64,7 +64,7 @@ const DevelopBusiness = () => {
         </div>
       </div>
       <div className="flex gap-3 justify-between text-center">
-        <Card className="flex flex-col items-center justify-center w-[280px] h-[180px] bg-green-400">
+        <Card className="flex flex-col items-center justify-center w-[280px] h-[180px] bg-green">
           <p className="text-4xl font-bold text-white">0</p>
           <p className="text-xl font-semibold text-white">E-Commerce</p>
         </Card>
